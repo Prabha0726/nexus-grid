eb63a69be9f0594e0b51dca4a540b5aa92368afd
Developer <dev@example.com>
2026-02-02 14:42:39 +0530

    Deployment ready: Added Dockerfile, Render config, and user avatars

--- chat_project/asgi.py
+++ chat_project/asgi.py
@@ -1,16 +1,17 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat_project.settings")
+django.setup()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 import chat.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat_project.settings")
-
-django_asgi_app = get_asgi_application()
-
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
+    "http": get_asgi_application(),
     "websocket": AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter(chat.routing.websocket_urlpatterns)
